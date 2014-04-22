841ebb6a4de70db2cf11f41915119e3442564187
RBertram <robin.bertram@gmail.com>
2014-04-22 20:03:33 +0200

    Initial upload.

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/* config */
+require('config.php');
+
+/* libs */
+require('vendors/framework/logging/Log.php');
+require('vendors/framework/debugging/Debug.php');
+require('vendors/framework/routing/Router.php');
+require('vendors/framework/database/Database.php');
+
+/* controllers */
+require('app/controllers/RootController.php');
+require('app/controllers/UsersController.php');
+require('app/controllers/HomeController.php');
+
+/* Assert options */
+assert_options(ASSERT_ACTIVE, 1);
+assert_options(ASSERT_WARNING, 0);
+assert_options(ASSERT_QUIET_EVAL, 1);
+assert_options(ASSERT_CALLBACK, '\Framework\Debugging\Debug::assertCallback');
+
+/* Application */
+try {
+    require('app/routes.php');
+} catch (Exception $ex) {
+    \Framework\Debugging\Debug::Shout($ex);
+}
