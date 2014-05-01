145458db625509670bcd752aafdacd233cbf7520
RBertram <robin.bertram@gmail.com>
2014-05-01 22:51:24 +0200

    Added a few database parameters and changed some values.

--- config.php
+++ config.php
@@ -1,14 +1,16 @@
 <?php
 
 $config = [
-    "path"  => "/1DV42E",
-    "logs" => [
-        "error" => "logs/error.log"
+    'path' => '/1DV42E',
+    'logs' => [
+        'error' => 'logs/error.log'
     ],
-    "database" => [
-        "host"  => "localhost",
-        "user"  => "root",
-        "pass"  => "",
-        "db"    => "wiho"
+    'database' => [
+        'host'    => 'localhost',
+        'user'    => 'root',
+        'pass'    => '',
+        'db'      => 'rabattrea',
+        'driver'  => 'mysql',
+        'charset' => 'utf8'
     ]
 ];
