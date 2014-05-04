4ea9c9bcfbf91602fcb60dcd79b50cf5272195eb
RBertram <robin.bertram@gmail.com>
2014-05-04 19:53:58 +0200

    Refactored some code and added some comments.

--- vendors/framework/database/OrderBy.php
+++ vendors/framework/database/OrderBy.php
@@ -1,25 +1,61 @@
 <?php namespace Framework\Database;
 
+use \Exception;
+
 class OrderBy {
+    /**
+     * The state of the order.
+     * @var  const
+     */
     const ASC = 'Asc';
     const DESC = 'Desc';
 
-    private $column;
+    /**
+     * Column.
+     * @var  string
+     */
+    private $column = '';
+
+    /**
+     * Order.
+     * @var  const
+     */
     private $order;
 
-    public function __construct($column, $order) {
+    /**
+     * Construct.
+     * @param  string  $column
+     * @param  const   $order
+     */
+    public function __construct($column, $order=self::ASC) {
         $this->setColumn($column);
         $this->setOrder($order);
     }
 
+    /**
+     * Set column.
+     * @param   string  $column
+     * @return  void
+     */
     private function setColumn($column) {
+        assert(is_string($column), 'Column must be a string.');
         $this->column = $column;
     }
 
+    /**
+     * Get column.
+     * @return  string
+     */
     public function getColumn() {
         return $this->column;
     }
 
+    /**
+     * Set order.
+     * @param   const  $order
+     * @return  void
+     * @throws  Exception
+     */
     private function setOrder($order) {
         switch ($order) {
             case self::ASC:
@@ -29,10 +65,14 @@ class OrderBy {
                 $this->order = self::DESC;
                 break;
             default:
-                $this->order = self::ASC;
+                throw new Exception("Incorrect order.");
         }
     }
 
+    /**
+     * Get order.
+     * @return  string
+     */
     public function getOrder() {
         return $this->order;
     }
