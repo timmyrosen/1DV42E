b9830a859499a82a72a45df99495443e5606f7f7
RBertram <robin.bertram@gmail.com>
2014-05-04 21:42:38 +0200

    Added a session handler.

---
+++ vendors/framework/session/Session.php
@@ -0,0 +1,67 @@
+<?php namespace Framework\Session;
+
+\Exception;
+
+class Session {
+    /**
+     * Add a value to a session.
+     * @param   string  $key
+     * @param   mixed  $value
+     * @return  void
+     */
+    public static function put($key, $value) {
+        assert(is_string($key), 'Key must be a string.');
+        assert(is_null($value), 'A value can\'t be null.');
+        $_SESSION[$key] = $value;
+    }
+
+    /**
+     * Get a session.
+     * @param   string  $key
+     * @return  mixed
+     * @throws  Exception
+     */
+    public static function get($key) {
+        if (!self::$has($key)) {
+            throw new Exception('The session key don\'t exist.');
+        }
+
+        return $_SESSION[$key];
+    }
+
+    /**
+     * Destroy a session.
+     * @return  void
+     * @throws  Exception
+     */
+    public static function destroy($key) {
+        if (!self::$has($key)) {
+            throw new Exception('The session key don\'t exist.');
+        }
+
+        unset($_SESSION[$key]);
+    }
+
+    /**
+     * Checks if a session is set.
+     * @param   string   $key
+     * @return  boolean
+     */
+    public static function has($key) {
+        assert(is_string($key), 'Key must be a string.');
+
+        if (isset($_SESSION[$key])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Destroy all sessions.
+     * @return  void
+     */
+    public static function flush() {
+        session_destroy();
+    }
+}
