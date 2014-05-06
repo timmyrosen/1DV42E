d08779b0dcb7e54db3cfbd869bee73ba679338c4
RBertram <robin.bertram@gmail.com>
2014-05-06 15:59:51 +0200

    Moved all includes to bootstrap.

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
 <?php
+use Framework\Routing\Router;
+
 try {
     session_start();
 
-    /* libs */
-    require('vendors/framework/config/Config.php');
-    require('vendors/framework/logging/Log.php');
-    require('vendors/framework/debugging/Debug.php');
-    require('vendors/framework/routing/Router.php');
-    require('vendors/framework/database/Database.php');
-    require('vendors/framework/session/Session.php');
-    require('vendors/framework/cookie/Cookie.php');
-
-    /* controllers */
-    require('app/controllers/UsersController.php');
-    require('app/controllers/StoresController.php');
-    require('app/controllers/HomeController.php');
+    require('vendors/framework/bootstrap.php');
 
     /* Assert options */
     assert_options(ASSERT_ACTIVE, 1);
@@ -22,8 +12,7 @@ try {
     assert_options(ASSERT_QUIET_EVAL, 1);
     assert_options(ASSERT_CALLBACK, '\Framework\Debugging\Debug::assertCallback');
 
-    require('app/filters.php');
-    require('app/routes.php');
+    Router::run();
 } catch (Exception $ex) {
     \Framework\Debugging\Debug::Shout($ex);
 }
---
+++ vendors/framework/bootstrap.php
@@ -0,0 +1,27 @@
+<?php namespace Framework;
+
+use Framework\Config\Config;
+
+require('vendors/framework/config/Config.php');
+require('vendors/framework/autoloading/Autoloader.php');
+require('vendors/framework/logging/Log.php');
+require('vendors/framework/debugging/Debug.php');
+require('vendors/framework/routing/Router.php');
+require('vendors/framework/database/DB.php');
+require('vendors/framework/session/Session.php');
+require('vendors/framework/cookie/Cookie.php');
+
+new Routing\Router();
+
+new Database\DB(
+    Config::get("database:driver"),
+    Config::get("database:host"),
+    Config::get("database:db"),
+    Config::get("database:user"),
+    Config::get("database:pass")
+);
+
+new Autoloading\Autoloader(
+    Config::get('autoloading:path'),
+    Config::get('autoloading:resources')
+);
