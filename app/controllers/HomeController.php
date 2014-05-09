6dae097f8f4f5c00254661100b87511dd9453024
RBertram <robin.bertram@gmail.com>
2014-05-09 18:15:03 +0200

    Added some example data.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,7 +4,7 @@ use Framework\View\View;
 
 class HomeController {
     public function index() {
-        return View::render('test');
+        return View::render('client/test');
         return View::json(Router::getRoutes());
         return View::abort(404, 'Sidan finns inte');
     }
