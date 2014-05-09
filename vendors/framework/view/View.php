5d12e888cae19566a5e4fdfdb2eacf87f357cc81
RBertram <robin.bertram@gmail.com>
2014-05-09 18:13:18 +0200

    Refactored some code and added support for more functionality.

--- vendors/framework/view/View.php
+++ vendors/framework/view/View.php
@@ -1,5 +1,7 @@
 <?php namespace Framework\View;
 
+// @todo  refactor this code, maybe create a separate template class
+
 class View {
     /**
      * Stores the instance of the class.
@@ -31,18 +33,53 @@ class View {
      * @todo    Build or include a template engine.
      */
     public static function render($page, $options=array()) {
-        $response = file_get_contents(self::$viewPath.'/'.$page.'.html');
+        $html = self::load($page);
 
-        if (preg_match('/\{{\s?@extends\s?=\s?\'(.+)\'\s?}}/', $response, $matches)) {
-            $partial = $response;
-            $extends = array_shift($matches);
-            $partial = str_replace($extends, '', $partial);
+        if (preg_match('/@extends\(\'(.+)\'\)/', $html, $matches)) {
+            $partial = $html;
+            $extends = $matches[1];
 
-            $master = file_get_contents(self::$viewPath.'/'.$matches[0].'.html');
-            $response = preg_replace('/\{{\s?@content\s?}}/', $partial, $master);
+            $master = self::load($extends);
+
+            preg_match_all('/@section\(\'(.+?)\'\)(.+?)@stop/s', $partial, $matches);
+
+            array_shift($matches);
+
+            $keys = $matches[0];
+            $values = $matches[1];
+
+            for ($i=0; $i<count($keys); $i++) {
+                $master = preg_replace('/@render\(\''.$keys[$i].'\'\)/', $values[$i], $master);
+            }
+
+            $html = $master;
         }
 
-        return $response;
+        return $html;
+    }
+
+    /**
+     * Loads a specific page.
+     * Uses output buffering strategy to retrieve
+     * the content of the page as a string and
+     * still executes the PHP code within.
+     * @param   string  $page
+     * @return  string
+     */
+    private static function load($page) {
+        ob_start();
+        require(self::$viewPath.'/'.$page.'.html');
+        $html = ob_get_clean();
+
+        if (preg_match_all('/@include\(\'(.+?)\'\)/', $html, $matches)) {
+            $keys = $matches[0];
+            $values = $matches[1];
+            for ($i=0; $i<count($keys); $i++) {
+                $include = self::load($values[$i]);
+                $html = str_replace($keys[$i], $include, $html);
+            }
+        }
+        return $html;
     }
 
     /**
