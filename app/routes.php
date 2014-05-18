3c623a4ca194d959e0c292d6f48105e161f082ab
Robin Bertram <robin.bertram@gmail.com>
2014-05-18 19:03:38 +0200

    Modified some paths related to the default pages.

--- app/routes.php
+++ app/routes.php
@@ -1,11 +1,11 @@
 <?php
 use Webbins\Routing\Router;
 
-Router::get("/", "HomeController:index");
+Router::get('/', 'HomeController:index');
 
-Router::resource("stores", "StoresController");
+Router::resource('stores', 'StoresController');
 
 // contains all available routes
-Router::get("routes", function() {
-    return Webbins\View\View::render('routes');
+Router::get('routes', function() {
+    return Webbins\View\View::render('webbins/routes');
 });
