1c2968b3a977374ce89f8f0d8d07a7a97f12873f
Robin Bertram <robin.bertram@gmail.com>
2014-05-19 11:06:26 +0200

    Now supports external links.

--- vendors/webbins/view/Compiler.php
+++ vendors/webbins/view/Compiler.php
@@ -277,6 +277,6 @@ class Compiler {
      *  @return  string
      */
     private function absolutePaths($code) {
-        return preg_replace('/(href|src)=([\"|\'])(.+?)([\"|\'])/', '$1=$2/'.$this->basePath.'/$3$4', $code);
+        return preg_replace('/(href|src)=([\"|\'])(?!http:\/\/|https:\/\/|\/\/)(.+?)([\"|\'])/', '$1=$2/'.$this->basePath.'/$3$4', $code);
     }
 }
