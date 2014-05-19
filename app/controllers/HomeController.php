27c7b440b0703bd402bb93cf69bac93e87b9580f
Robin Bertram <robin.bertram@gmail.com>
2014-05-19 09:27:07 +0200

    Update.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,6 +4,5 @@ use Webbins\View\View;
 class HomeController {
     public function index() {
         return View::render('client/home', array('name' => 'Robin'));
-        return View::abort(404, 'The page wasn\'t found.');
     }
 }
